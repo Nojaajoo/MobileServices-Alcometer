8d4d397a2b925518670b3933143e2347152d52e1
Joona Oja <n0ojjo00@students.oamk.fi>
2022-02-14 16:09:44 +0200

    UI skeleton ready

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, ScrollView, Button } from 'react-native';
 import Stylesheet from './styles/Styles';
 import RadioButton from './components/RadioButton';
 import React, { useState } from 'react';
+import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
 
-  const [test,setTest] = useState('No selection');
+  const [gender,setGender] = useState('No selection');
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [result, setResult] = useState(0);
 
-  const testValues = [
+  const bottlesAmount=Array();
+  bottlesAmount.push({label: '1 bottle', value: 1});
+  bottlesAmount.push({label: '2 bottles', value: 2});
+  bottlesAmount.push({label: '3 bottles', value: 3});
+  bottlesAmount.push({label: '4 bottles', value: 4});
+  bottlesAmount.push({label: '5 bottles', value: 5});
+
+  const hours=Array();
+  hours.push({label: '1 hour', value: 1});
+  hours.push({label: '2 hours', value: 2});
+  hours.push({label: '3 hours', value: 3});
+  hours.push({label: '4 hours', value: 4});
+  hours.push({label: '5 hours', value: 5});
+
+  const genderValues = [
     {
       label: 'Female',
       value: 1
@@ -21,10 +39,58 @@ export default function App() {
 
 
   return (
-    <View style={Stylesheet.container}>
-      <RadioButton options={testValues} onPress={(value) => {setTest(value)}} />
-      <Text>{test}</Text>
+    <ScrollView style={Stylesheet.container}>
+      <Text>Alcometer</Text>
+      <Text>Weight</Text>
+      <TextInput 
+        placeholder='Input your weight in kilograms.'
+        keyboardType='number-pad'
+      />
+      <View style={Stylesheet.separator} />
+      
+      <Text>Bottles</Text>
+      <View>
+        <Picker style={Stylesheet.bottlesPicker}
+            onValueChange={(itemValue) => setBottles(itemValue)}
+            selectedValue={bottles}
+          >
+            {bottlesAmount.map((bottles,index) => (
+              <Picker.Item key={index} label={bottles.label} value={bottles.value}/>
+            ))}
+        </Picker>
+        {/* testing */}
+        <Text>{bottles}</Text>
+      </View>
+
+      <Text>Time</Text>
+      <View>
+        <Picker style={Stylesheet.bottlesPicker}
+            onValueChange={(itemValue) => setTime(itemValue)}
+            selectedValue={time}
+          >
+            {hours.map((hours,index) => (
+              <Picker.Item key={index} label={hours.label} value={hours.value}/>
+            ))}
+        </Picker>
+        {/* testing */}
+        <Text>{time}</Text>
+      </View>
+      
+      <Text>Gender</Text>
+      <View>
+        <RadioButton options={genderValues} onPress={(value) => {setGender(value)}} />
+        {/* testing */}
+        <Text>{gender}</Text>
+      </View>
+
+      {/* Result */}
+      <View>
+        <Text style={Stylesheet.result} >{result}</Text>
+      </View>
+
+      <Button title='Calculate' />
+      
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
--- styles/Styles.js
+++ styles/Styles.js
@@ -3,8 +3,24 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#f3f3f3',
+        margin: 20,
+        marginTop: 50,
+        padding: 20,
+        borderRadius: 20,
     },
+    separator: {
+        marginTop: 5,
+        borderBottomColor: 'darkgrey',
+        borderBottomWidth: 1,
+        alignSelf: 'stretch',
+        marginBottom: 5,
+    },
+    bottlesPicker: {
+        margin: 10,
+    },
+    result: {
+        fontSize: 40,
+        alignSelf: 'center'
+    }
 });
